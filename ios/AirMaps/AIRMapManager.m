a48fa5853a665de60a8df1775fc6d87de61bc2ea
edgar wang <edgarwang95@gmail.com>
2017-06-14 14:18:52 -0400

    added map ready callback

--- ios/AirMaps/AIRMapManager.m
+++ ios/AirMaps/AIRMapManager.m
@@ -86,6 +86,7 @@ RCT_EXPORT_VIEW_PROPERTY(minDelta, CGFloat)
 RCT_EXPORT_VIEW_PROPERTY(legalLabelInsets, UIEdgeInsets)
 RCT_EXPORT_VIEW_PROPERTY(mapType, MKMapType)
 RCT_EXPORT_VIEW_PROPERTY(onChange, RCTBubblingEventBlock)
+RCT_EXPORT_VIEW_PROPERTY(onMapReady, RCTBubblingEventBlock)
 RCT_EXPORT_VIEW_PROPERTY(onPanDrag, RCTBubblingEventBlock)
 RCT_EXPORT_VIEW_PROPERTY(onPress, RCTBubblingEventBlock)
 RCT_EXPORT_VIEW_PROPERTY(onLongPress, RCTBubblingEventBlock)
@@ -644,6 +645,8 @@ static int kDragCenterContext;
 {
     [mapView finishLoading];
     [mapView cacheViewIfNeeded];
+
+    mapView.onMapReady(@{@"finished": @true});
 }
 
 #pragma mark Private
